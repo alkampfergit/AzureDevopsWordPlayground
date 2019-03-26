e4d06560c070a72fc44ac51ba342d6678b2795af
Gian Maria Ricci <alkampfer@nablasoft.com>
2019-03-26 21:21:46 +0100

    Removed html table code from work item description
    Actually if a description contains html code with a table
    generated word document is invalid and cannot be opened.

--- src/WordExporter.Core/WorkItems/Extensions.cs
+++ src/WordExporter.Core/WorkItems/Extensions.cs
@@ -25,7 +25,8 @@ namespace WordExporter.Core.WorkItems
             retValue["title"] = workItem.Title;
             retValue["id"] = workItem.Id;
             //Some help to avoid forcing the user to use System.AssignedTo etc for most commonly used fields.
-            retValue["description"] = new HtmlSubstitution(workItem.EmbedHtmlContent(workItem.Description));
+            retValue["description"] = new HtmlSubstitution(workItem.GenerateHtmlForWordEmbedding(workItem.Description));
+            retValue["description.txt"] = GetTxtFromHtmlContent(workItem.Description);
             retValue["assignedto"] = workItem.Fields["System.AssignedTo"].Value?.ToString() ?? String.Empty;
             retValue["createdby"] = workItem.Fields["System.CreatedBy"].Value?.ToString() ?? String.Empty;
 
@@ -86,52 +87,106 @@ namespace WordExporter.Core.WorkItems
             return field.Value.ToString();
         }
 
-        public static String EmbedHtmlContent(this WorkItem workItem, String htmlContent)
+        /// <summary>
+        /// This is not an extension method because it does not use WorkItem
+        /// </summary>
+        /// <param name="workItem"></param>
+        /// <param name="htmlContent"></param>
+        /// <returns></returns>
+        public static String GetTxtFromHtmlContent(String htmlContent)
         {
             HtmlDocument doc = new HtmlDocument();
             doc.LoadHtml(htmlContent);
+            return doc.DocumentNode.InnerText;
+        }
 
-            var images = doc.DocumentNode.SelectNodes("//img");
-            if (images != null)
+        public static String GenerateHtmlForWordEmbedding(this WorkItem workItem, String htmlContent)
+        {
+            try
             {
-                foreach (var image in images)
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(htmlContent);
+
+                var images = doc.DocumentNode.SelectNodes("//img");
+                if (images != null)
                 {
-                    //need to understand if it is in base 64 or no, if the answer is no, we need to embed image
-                    var src = image.GetAttributeValue("src", "");
-                    if (!String.IsNullOrEmpty(src))
+                    foreach (var image in images)
                     {
-                        if (src.Contains("base64")) // data:image/jpeg;base64,
+                        //need to understand if it is in base 64 or no, if the answer is no, we need to embed image
+                        var src = image.GetAttributeValue("src", "");
+                        if (!String.IsNullOrEmpty(src))
                         {
-                            //image already embedded
-                            Log.Debug("found image in html content that was already in base64");
-                        }
-                        else
-                        {
-                            Log.Debug("found image in html content that point to external image {src}", src);
-                            //is it a internal attached images?
-                            var match = Regex.Match(src, @"FileID=(?<id>\d*)");
-                            if (match.Success)
+                            if (src.Contains("base64")) // data:image/jpeg;base64,
                             {
-                                var attachment = workItem.Attachments
-                                    .OfType<Attachment>()
-                                    .FirstOrDefault(_ => _.Id.ToString() == match.Groups["id"].Value);
-                                if (attachment != null)
+                                //image already embedded
+                                Log.Debug("found image in html content that was already in base64");
+                            }
+                            else
+                            {
+                                Log.Debug("found image in html content that point to external image {src}", src);
+                                //is it a internal attached images?
+                                var match = Regex.Match(src, @"FileID=(?<id>\d*)");
+                                if (match.Success)
                                 {
-                                    //ok we can embed in the image as base64
-                                    WorkItemServer wise = workItem.Store.TeamProjectCollection.GetService<WorkItemServer>();
-                                    var downloadedAttachment = wise.DownloadFile(attachment.Id);
-                                    byte[] byteContent = File.ReadAllBytes(downloadedAttachment);
-                                    String base64Encoded = Convert.ToBase64String(byteContent);
-                                    var newSrcValue = $"data:image/{attachment.Extension.Trim('.')};base64,{base64Encoded}";
-                                    image.SetAttributeValue("src", newSrcValue);
+                                    var attachment = workItem.Attachments
+                                        .OfType<Attachment>()
+                                        .FirstOrDefault(_ => _.Id.ToString() == match.Groups["id"].Value);
+                                    if (attachment != null)
+                                    {
+                                        //ok we can embed in the image as base64
+                                        WorkItemServer wise = workItem.Store.TeamProjectCollection.GetService<WorkItemServer>();
+                                        var downloadedAttachment = wise.DownloadFile(attachment.Id);
+                                        byte[] byteContent = File.ReadAllBytes(downloadedAttachment);
+                                        String base64Encoded = Convert.ToBase64String(byteContent);
+                                        var newSrcValue = $"data:image/{attachment.Extension.Trim('.')};base64,{base64Encoded}";
+                                        image.SetAttributeValue("src", newSrcValue);
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            }
 
-            return doc.DocumentNode.OuterHtml;
+                CollapseNodeWithText(doc, "table");
+
+                return doc.DocumentNode.OuterHtml;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Unable to generate emdettable html: {htmlContent}", htmlContent);
+                return "Error converting HTML text: " + ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// Given an HTML tag that is not permitted as embed in word document,
+        /// we can simply collapse the tag with plain text content
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="nodeName"></param>
+        private static void CollapseNodeWithText(HtmlDocument doc, string nodeName)
+        {
+            var nodes = doc.DocumentNode.SelectNodes($"//{nodeName}");
+            if (nodes != null)
+            {
+                foreach (var node in nodes.ToList())
+                {
+                    node.ParentNode.ReplaceChild(HtmlNode.CreateNode(node.InnerText), node);
+                }
+            }
+        }
+
+        private static void RemoveAttributeFromDocument(HtmlDocument doc, string classn)
+        {
+            var classAttribute = doc.DocumentNode.SelectNodes($"//*[@{classn}]");
+            if (classAttribute != null)
+            {
+                foreach (var nodeWithClassAttribute in classAttribute)
+                {
+                    nodeWithClassAttribute.Attributes[classn].Remove();
+                }
+            }
         }
     }
 }
+    ;
